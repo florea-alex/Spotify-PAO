1f001f43371846f513545a993036a41f42a45003
Alexandra Florea <98469752+florea-alex@users.noreply.github.com>
2022-05-23 10:25:44 +0300

    Delete src directory

--- src/Services/ISubscriberServices.java
+++
@@ -1,9 +0,0 @@
-package Repositories;
-
-import Spotify.premium.Subscriber;
-
-public interface ISubscriberServices {
-    public boolean  login(String email, String password);
-
-    public void register(Subscriber subscriber);
-}
